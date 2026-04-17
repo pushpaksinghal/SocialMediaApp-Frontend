92fa18525994af668e32bd09a1cbf4bd4d2ef936
pushpaksinghal <pushpaksinghal1234@gmail.com>
2026-04-17 11:34:13 +0530

    [pushpak singhal]Add.

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,10 +1,10 @@
 export const environment = {
   production: true,
-  authApi:    'https://connectsphere-auth-api.onrender.com/api/auth',
-  postApi:    'https://connectsphere-post-api.onrender.com/api/posts',
-  feedApi:    'https://commentsphere-feed-api.onrender.com/api/feed',
-  likeApi:    'https://commentsphere-like-api.onrender.com/api/likes',
-  commentApi: 'https://connectsphere-comment-api.onrender.com/api/comments',
-  followApi:  'https://connectsphere-follow-api.onrender.com/api/follows',
-  notifApi:   'https://connectsphere-notif-api.onrender.com/api/notifications',
+  authApi:    'https://connectsphere-gateway-api.onrender.com/api/auth',
+  postApi:    'https://connectsphere-gateway-api.onrender.com/api/posts',
+  feedApi:    'https://connectsphere-gateway-api.onrender.com/api/feed',
+  likeApi:    'https://connectsphere-gateway-api.onrender.com/api/likes',
+  commentApi: 'https://connectsphere-gateway-api.onrender.com/api/comments',
+  followApi:  'https://connectsphere-gateway-api.onrender.com/api/follows',
+  notifApi:   'https://connectsphere-gateway-api.onrender.com/api/notifications',
 };
